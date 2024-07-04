7989e068f1cc1b38f49fcf8a020965b7680b1c7d
Suryansh Chourasia <suryanshchourasia12@gmail.com>
2024-07-04 18:41:00 +0530

    Signup route completed tested with postman

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,36 @@
 import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+import {decode,sign,verify } from 'hono/jwt'
+// whenever you want to use env variables you need to define them in the type of the app
+const app = new Hono<{
+  Bindings:{
+    DATABASE_URL: string,
+      JWT_SECRET: string,
+  },
+}>()
 
-const app = new Hono()
 
-app.post('/api/v1/signup', (c) => {
-  return c.text('Hello Hono!')
+//c is context which has request and response object 
+app.post('/api/v1/signup', async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+}).$extends(withAccelerate())
+
+  const body = await c.req.json();
+	try {
+		const user = await prisma.user.create({
+			data: {
+				email: body.email,
+				password: body.password
+			}
+		});
+	
+		const token = await sign({id: user.id}, c.env.JWT_SECRET)
+    return c.json({jwt:token})
+	} catch(e) {
+		return c.status(403);
+	}
 })
 app.post('/api/v1/signin', (c) => {
   return c.text('Hello Hono!')
