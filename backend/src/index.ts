b67b198a988bf38199ff025116b32aa605a2dc7d
Suryansh Chourasia <suryanshchourasia12@gmail.com>
2024-07-04 20:15:36 +0530

    Add authorization middleware for blog API endpoints Added Sign In route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,7 +10,19 @@ const app = new Hono<{
   },
 }>()
 
-
+app.use('/api/v1/blog/*', async (c, next) => {
+  //get the header verify the header if header is correct proceed if not we notify the user
+  const header = c.req.header('authorization') || ''  ; 
+  const token = header.split(' ')[1]
+  const response = await verify(token, c.env.JWT_SECRET)
+  if (response.id) {
+     next()
+  }else{
+     c.status(403)
+     return c.json({error: 'unauthorized'})
+  }
+  await next()
+})
 //c is context which has request and response object 
 app.post('/api/v1/signup', async (c) => {
   const prisma = new PrismaClient({
@@ -32,9 +44,29 @@ app.post('/api/v1/signup', async (c) => {
 		return c.status(403);
 	}
 })
-app.post('/api/v1/signin', (c) => {
-  return c.text('Hello Hono!')
+
+app.post('/api/v1/signin', async (c) => {
+	const prisma = new PrismaClient({
+		datasourceUrl: c.env?.DATABASE_URL	,
+	}).$extends(withAccelerate());
+
+	const body = await c.req.json();
+	const user = await prisma.user.findUnique({
+		where: {
+			email: body.email,
+      password: body.password
+		}
+	});
+
+	if (!user) {
+		c.status(403);
+		return c.json({ error: "user not found" });
+	}
+
+	const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
+	return c.json({ jwt });
 })
+
 app.post('/api/v1/blog', (c) => {
   return c.text('Hello Hono!')
 })
