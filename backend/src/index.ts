c0ce78b8c52ddb1e27114f4bbf67a7baa91e1ca4
Suryansh Chourasia <suryanshchourasia12@gmail.com>
2024-07-05 20:39:38 +0530

    Sign in Sign up page added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,5 @@
 import { Hono } from 'hono'
-import { PrismaClient } from '@prisma/client/edge'
-import { withAccelerate } from '@prisma/extension-accelerate'
-import {decode,sign,verify } from 'hono/jwt'
+
 import { userRouter } from './routes/user'
 import { blogRouter } from './routes/blog'
 // whenever you want to use env variables you need to define them in the type of the app
