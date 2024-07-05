95bf831cc551c455d4b145a1774d3d06956592c5
Suryansh Chourasia <suryanshchourasia12@gmail.com>
2024-07-05 23:53:01 +0530

    Update backend and frontend routes and frontend pages

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,5 @@
 import { Hono } from 'hono'
-
+import { cors } from 'hono/cors'
 import { userRouter } from './routes/user'
 import { blogRouter } from './routes/blog'
 // whenever you want to use env variables you need to define them in the type of the app
@@ -10,6 +10,7 @@ const app = new Hono<{
   },
 }>()
 
+app.use('/api/*', cors())
 app.route('/api/v1/user', userRouter)
 app.route('/api/v1/blog', blogRouter)
 
